32ce794432a839a3e12807cc8e89d4666cd7ba34
akeshri <ashwini.nitdgp@gmail.com>
2020-06-28 16:25:40 +0530

    Add files via upload

---
+++ src/main/java/com/article/hexagonal/architecture/App.java
@@ -0,0 +1,18 @@
+package com.article.hexagonal.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AshwiniKeshri
+ *
+ */
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
